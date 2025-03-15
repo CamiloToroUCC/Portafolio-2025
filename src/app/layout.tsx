78614b9fa076916409e49962e7c8f650cf32d860
Camilo Toro <camilotoro@email.edu.co>
2025-03-15 17:46:38 -0500

    Se realiza Taller Paleta de Colores en Portafolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
-  title: "Portafolio de Camilo Toro ",
-  description: "Este es mi portafolio para mostrar el portafolio de mis habilidades , actitudes  y aptitudes  como ingeniero de Software en formacion",
+  title: "Portafolio de Camilo Toro",
+  description: "Este es mi portafolio para mostrar el portafolio de mis habilidades, actitudes y aptitudes como ingeniero de Software en formación",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
